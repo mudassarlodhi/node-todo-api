2bd7d57e3b14159737a0b8b44078dacde348b073
Mudasser Lodhi <mudasserlodhi@yahoo.com>
2019-01-17 02:09:55 +0500

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ var {User} = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
-app.post("/todos/" , (req, res)=>{
+app.post("/todos" , (req, res)=>{
+
     var todo = new Todo({
       text: req.body.text,
     });
@@ -23,3 +24,5 @@ app.post("/todos/" , (req, res)=>{
 });
 
 app.listen(3000 , ()=>{console.log("Started on port 3000")});
+
+module.exports = {app}
