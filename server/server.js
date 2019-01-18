01d6a4787163a3339e5b3025216c8c833f4d95f0
Mudasser Lodhi <mudasserlodhi@yahoo.com>
2019-01-19 03:12:06 +0500

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require("mongodb");
 
 var app = express();
 app.use(bodyParser.json());
@@ -27,7 +28,31 @@ app.get('/todos' , (req, res)=>{
   Todo.find().then(
     (todos)=>res.status(200).send({todos})
   ).catch((e)=>res.status(400).send(e))
-})
+});
+
+
+app.get("/todos/:id" , (req, res)=>{
+  var id = req.params.id;
+
+   if(!ObjectID.isValid(id))
+   {
+     res.status(404).send();
+   }
+   else
+   {
+     Todo.findById(id).then((todo)=>{
+       if(todo)
+       {
+         return res.status(200).send({todo});
+       }
+       res.status(404).send();
+     }).catch(e=>res.send.status(400).send());
+   }
+  // Todo.findById(id).then(()=>{
+  //
+  // })
+
+});
 
 app.listen(3000 , ()=>{console.log("Started on port 3000")});
 
